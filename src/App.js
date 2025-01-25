df919c08c320a98f9ecd863c6e32b3b187d5f6e0
syhsk21 <syhsk.21@gmail.com>
2025-01-25 18:49:20 +0900

    リセットボタン追加

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import { Header } from "./components/header/Header";
 import { TaskCards } from "./components/task/TaskCards";
 
 function App() {
+  const resetLocalStorage = () => {
+    localStorage.clear(); // localStorageをすべてクリア
+    window.location.reload(); // ページをリロードして状態を反映させる
+  };
+
   return (
     <div className="app">
       <Header />
+      <button onClick={resetLocalStorage} className="reset-button">
+        リセット
+      </button>
       <TaskCards />
     </div>
   );
